eed8b482314729a2c6b8d365cc96156b790767df
Phillip Youn <phillipyoun@gmail.com>
2020-03-02 11:40:25 -0800

    Full board works, add message to queue

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,16 +1,33 @@
 console.log('APP is connected!')
 
 
-
-// document.getElementById("tr").addEventListener("click", console.log('did click!'));
-
-// warning for starting player (we set it to x)
+// IF we want a warning for starting player (we set it to x)
 function startGame() {
-  document.turn = 'X';
-
-  setMessage(document.turn + ' gets to start');
+  document.player = 'X';
+  setMessage(document.player + ' gets to start');
 }
 
 function setMessage(msg) {
   document.getElementById('message').innerText = msg;
+}
+
+//to simplify, let's just start by hardcoding the startingplayer as X
+// document.player = 'X';
+// function alternates between x and o depending on who's turn it is
+function changePlayer() {
+  if (document.player === 'X'){
+    document.player = 'O'
+  }else{
+    document.player = 'X'
+  }
+  setMessage('Go ' + document.player + '!')
+}
+
+//if this is called, change the innerText to whichever player
+function nextMove(inputBox){
+  if (inputBox.innerText){
+    return
+  }
+  inputBox.innerText = document.player;
+  changePlayer();
 }
