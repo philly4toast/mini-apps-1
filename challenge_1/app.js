3faacc4e77b40ef3b6ddb3d862df43248232611e
Phillip Youn <phillipyoun@gmail.com>
2020-03-02 11:16:44 -0800

    Table out, style box made

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,16 @@
+console.log('APP is connected!')
 
 
+
+// document.getElementById("tr").addEventListener("click", console.log('did click!'));
+
+// warning for starting player (we set it to x)
+function startGame() {
+  document.turn = 'X';
+
+  setMessage(document.turn + ' gets to start');
+}
+
+function setMessage(msg) {
+  document.getElementById('message').innerText = msg;
+}
