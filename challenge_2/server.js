80a88a17ea5547ea5c788b6061188faef7d360a5
Phillip Youn <phillipyoun@gmail.com>
2020-03-03 11:10:39 -0800

    Set up server environment need more tools
    Going to do some research on connecting front end to server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 5000;
 
+app.use(express.static('client'))
 
+app.listen(port, () => console.log(`listening on port ${port}`));
+
+// app.get('/express_backend', (req, res) => {
+//   res.send({express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT'});
+// })
